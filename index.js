45a76224a420b18a7bfb6bc7787c140cda606cc6
Replit user <>
2022-12-27 07:23:38 +0000

    Code changes from 2022-12-27_07:23:38

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ function configureBot(bot) {
     await craft('crafting_table');
 
     let blocks = bot.findBlocks({blockNames: ['grass_block']});
-    bot.chat(blocks[0]);
+    bot.chat(blocks[0].toString());
     let surfaceBlocks = blocks.filter((b) => bot.mineflayer().blockAt(b.position.offset(0, 1, 0)).type === 0);
     let pick = surfaceBlocks[0];
     
